e814376d3ba9ff963170fadce577dbc82794f62a
朱云飞 <2279948211@qq.com>
2023-02-16 23:38:22 +0800

    feat: 虚拟模块加载

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import testHookPlugin from "./test-hooks-plugin";
+import virtual from "./plugins/virtual-module";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), virtual()],
+});
